8d0bb57b3247d16ae33123c4cebbd5a8c21a7cc2
Dilum Aluthge <dilum@aluthge.com>
2020-05-02 03:27:56 +0000

    Update HealthBase.jl

--- src/HealthBase.jl
+++ src/HealthBase.jl
@@ -1,12 +1,33 @@
 module HealthBase
 
+"""
+    fit
+"""
 function fit end
+
+"""
+    fit!
+"""
 function fit! end
 
+"""
+    predict
+"""
 function predict end
+
+"""
+    predict!
+"""
 function predict! end
 
+"""
+    predict_proba
+"""
 function predict_proba end
+
+"""
+    predict_proba!
+"""
 function predict_proba! end
 
 end # module
