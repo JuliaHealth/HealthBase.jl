7a4bc66decc403683a77249b835e8f1d5d7f5f68
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-04 00:05:01 +0000

    build based on 69049c5

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HealthBase.jl","page":"Home","title":"HealthBase.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"This is the documentation for HealthBase.jl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HealthBase]","category":"page"}]
+}
