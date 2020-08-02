48b4ad52b09ffbbf405c0dd950dcb6a536f31844
Dilum Aluthge <dilum@aluthge.com>
2020-08-02 08:19:26 +0000

    Update some docstrings to make them more general

--- src/HealthBase.jl
+++ src/HealthBase.jl
@@ -3,55 +3,31 @@ module HealthBase
 """
     AbstractHealthServer
 """
-abstract type AbstractHealthServer
-end
+abstract type AbstractHealthServer end
 
 """
     AbstractFHIRServer <: AbstractHealthServer
 """
-abstract type AbstractFHIRServer <: AbstractHealthServer
-end
+abstract type AbstractFHIRServer <: AbstractHealthServer end
 
 """
     AbstractHealthClient
-
-An `AbstractHealthClient` connects to a server.
-
-Generally, when constructing an instance of an `AbstractHealthClient`, you
-will pass the URL of the server's endpoint as an argument.
-
-Note: A single `AbstractHealthClient` connects to a single server. If you
-want to connect to multiple servers simultaneously, you will need to
-construct a different `AbstractHealthClient` for each server.
 """
-abstract type AbstractHealthClient
-end
+abstract type AbstractHealthClient end
 
 """
     AbstractFHIRClient <: AbstractHealthClient
-
-An `AbstractFHIRClient` connects to a FHIR server.
-
-Generally, when constructing an instance of an `AbstractFHIRClient`, you will
-pass the URL of the server's endpoint as an argument.
-
-Note: A single `AbstractFHIRClient` connects to a single FHIR server. If you
-want to connect to multiple FHIR servers simultaneously, you will need to
-construct a different `AbstractFHIRClient` for each FHIR server.
 """
-abstract type AbstractFHIRClient <: AbstractHealthClient
-end
+abstract type AbstractFHIRClient <: AbstractHealthClient end
 
 """
     AbstractHealthInformationModel
 """
-abstract type AbstractHealthInformationModel
-end
+abstract type AbstractHealthInformationModel end
 
 """
     AbstractFHIRModel <: AbstractHealthInformationModel
 """
-abstract type AbstractFHIRModel <: AbstractHealthInformationModel
-end
+abstract type AbstractFHIRModel <: AbstractHealthInformationModel end
 
-end # module
+end # end module HealthBase
